cab71d38da96e0a8328284750ec136b94175ad01
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-12 11:26:35 -0800

    Create temporary coordinate information for main_board

--- modules/create.py
+++ modules/create.py
@@ -4,6 +4,7 @@ Kanon Nishiyama
 A1415217
 
 """
+from pprint import pprint
 
 
 def tutorial_board():
@@ -16,24 +17,23 @@ def tutorial_board():
     >>> tutorial_board() #doctest: +ELLIPSIS
     {(0, 0): {'name': 'The Dungeons - Hallway', 'look': 'There is no light inside the dark ...
     """
-    return {
-            (0, 0): {"name": "The Dungeons - Hallway",
+    return {(0, 0): {"name": "The Dungeons - Hallway",
                      "look": "There is no light inside the dark cavernous halls of the dungeons, "
                              "the only sliver of light pours in from the slits of the door which withholds "
-                             "your freedom. You see a glint of something in the corner of your eye. Maybe "
-                             "you could SCAVENGE to see what it is.",
+                             "your freedom.\n"
+                             "You see a glint of something in the corner of your eye.\n"
+                             "Maybe you could SCAVENGE to see what it is.",
                      "area": "safe",
-                     "scavenge": "none",
+                     "scavenge": "key",
                      "action": "none",
                      "walls": ["n", "s", "w"]},
             (0, 1): {"name": "The Dungeons - The Exit",
-                     "look": "The key to your freedom lies in front of you, locked and guarded from outside. "
-                             "Once you EXIT, you are sure to enter battle with the guards outside.",
+                     "look": "Your only exit lies in front of you, locked and guarded from outside.\n"
+                             "Once you EXIT, you are sure to enter battle with the guards.",
                      "area": "safe",
                      "scavenge": "none",
                      "action": "exit",
-                     "walls": ["n", "s", "e"]}
-            }
+                     "walls": ["n", "s", "e"]}}
 
 
 def main_board():
@@ -44,13 +44,163 @@ def main_board():
              containing the coordinates information
 
     >>> main_board() #doctest: +ELLIPSIS
-    {(0, 0): {'name': 'The Dungeons - Hallway', 'look': 'There is no light inside the dark ...
+    {(0, 0): {'name': 'The Dungeons - Hallway', 'look': 'What is left of the dungeons is a shell ...
     """
-    return {(0, 0): {}, (0, 1): {}, (0, 2): {}, (0, 3): {}, (0, 4): {},
-            (1, 0): {}, (1, 1): {}, (1, 2): {}, (1, 3): {}, (1, 4): {},
-            (2, 0): {}, (2, 1): {}, (2, 2): {}, (2, 3): {}, (2, 4): {},
-            (3, 0): {}, (3, 1): {}, (3, 2): {}, (3, 3): {}, (3, 4): {},
-            (4, 0): {}, (4, 1): {}, (4, 2): {}, (4, 3): {}, (4, 4): {}}
+    return {(0, 0): {"name": "The Dungeons - Hallway",
+                     "look": "What is left of the dungeons is a shell of what used to confine you. "
+                             "Only rats roam these halls now.",
+                     "area": "dungeon",
+                     "scavenge": "none",
+                     "action": "none",
+                     "walls": ["n", "s", "w"]},
+            (0, 1): {"name": "The Dungeons - The Exit",
+                     "look": "The slain bodies of the guards remain discarded next to the door.",
+                     "area": "safe",
+                     "scavenge": "none",
+                     "action": "none",
+                     "walls": ["n", "e"]},
+            (0, 2): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (0, 3): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (0, 4): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+
+            (1, 0): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (1, 1): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (1, 2): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (1, 3): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (1, 4): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+
+            (2, 0): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (2, 1): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (2, 2): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (2, 3): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (2, 4): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+
+            (3, 0): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (3, 1): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (3, 2): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (3, 3): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (3, 4): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+
+            (4, 0): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (4, 1): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (4, 2): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (4, 3): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []},
+            (4, 4): {"name": "",
+                     "look": "",
+                     "area": "",
+                     "scavenge": "",
+                     "action": "",
+                     "walls": []}}
 
 
 def main():
@@ -58,7 +208,9 @@ def main():
     Drive the program.
     """
     test = tutorial_board()
+    print(test[(0, 0)]["name"])
     print(test[(0, 0)]["look"])
+    print(test[(0, 0)]["area"])
 
 
 if __name__ == "__main__":
