4ce5672b92e5dabc588a9c67f28184be65216078
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-12 08:18:14 -0800

    Set up main and game function

---
+++ game.py
@@ -0,0 +1,23 @@
+"""
+
+Kanon Nishiyama
+A1415217
+
+"""
+
+
+def game():
+    """
+    Drive the game.
+    """
+
+
+def main():
+    """
+    Drive the program.
+    """
+    game()
+
+
+if __name__ == "__main__":
+    main()
