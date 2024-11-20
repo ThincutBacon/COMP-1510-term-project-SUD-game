5c1411c2e6eed1b6cfc3d2432ae2b6e9e5e25b6c
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-20 11:23:58 -0800

    Create doctests for each function

--- modules/create_character.py
+++ modules/create_character.py
@@ -4,6 +4,8 @@ Kanon Nishiyama
 A1415217
 
 """
+from pprint import pprint
+
 from modules import get
 
 
@@ -15,9 +17,12 @@ def name_kingdom(character):
     :precondition: character must be a dictionary from get.blank_character function
     :postcondition: change the value of key "kingdom" to player input
 
-    >>> test_character = get.blank_character()
-    >>> name_kingdom(test_character)
-    ''
+    >>> test_character = get.blank_character() #doctest: +SKIP
+    >>> name_kingdom(test_character) #doctest: +SKIP
+    What is the name of your kingdom?:
+    >>> player_input = "Hyrule" #doctest: +SKIP
+    >>> print(test_character["kingdom"]) #doctest: +SKIP
+    Hyrule
     """
     while True:
         kingdom_name = input("\n\n\nWhat is the name of your kingdom?: ").strip()
@@ -36,9 +41,14 @@ def name_character(character):
     :precondition: character must be a dictionary from get.blank_character function
     :postcondition: change the value of key "name" to player input
 
-    >>> test_character = get.blank_character()
-    >>> name_character(test_character)
-    ''
+    >>> test_character = get.blank_character() #doctest: +SKIP
+    >>> test_character["kingdom"] = "Hyrule" #doctest: +SKIP
+    >>> name_character(test_character) #doctest: +SKIP
+    As heir to the throne of HYRULE,
+    What is your name?:
+    >>> player_input = "Zelda" #doctest: +SKIP
+    >>> print(test_character["name"]) #doctest: +SKIP
+    Zelda
     """
     while True:
         print("\n\n\nAs heir to the throne of " + character["kingdom"].upper() + ",")
@@ -59,9 +69,38 @@ def choose_species(character):
     :postcondition: change the value of key "species" to player input
     :postcondition: change the values of character attributes based on chosen species
 
-    >>> test_character = get.blank_character()
-    >>> choose_species(test_character)
-    ''
+    >>> test_character = get.blank_character() #doctest: +SKIP
+    >>> choose_species(test_character) #doctest: +SKIP
+    ==========
+    HUMAN
+    "The most versatile of species."
+
+    ELF
+    "The graceful guardians of the forests."
+
+    DWARF
+    "Inhabitants of the deepest caves and the highest mountains."
+    ==========
+
+    Enter the species name for more information:
+    >>> player_input = "human" #doctest: +SKIP
+
+
+
+    ==========
+    HUMAN
+
+    Although average in most aspects, they possess strong survival
+    prowess and the ability to utilize items to their fullest potential.
+
+    Highest Attributes: ATK and LUK
+    Species Bonus: All items gain an additional +2 to their effects
+    ==========
+
+    Do you want to be a HUMAN? (y/n):
+    >>> player_input = "y" #doctest: +SKIP
+    >>> print(test_character["species"]) #doctest: +SKIP
+    human
     """
     confirm_species = False
     while not confirm_species:
@@ -97,9 +136,40 @@ def choose_class(character):
     :precondition: character must be a dictionary from get.blank_character function
     :postcondition: change the value of key "class" to player input
 
-    >>> test_character = get.blank_character()
-    >>> choose_species(test_character)
-    ''
+    >>> test_character = get.blank_character() #doctest: +SKIP
+    >>> choose_species(test_character) #doctest: +SKIP
+    ==========
+    WARRIOR
+    "For those who fight with endurance."
+
+    SCOUT
+    "For those who fight with deadly accuracy."
+
+    MAGE
+    "For those who fight by leveling the playing field."
+    ==========
+
+    Enter the class name for more information:
+    >>> player_input = "scout" #doctest: +SKIP
+
+
+
+    ==========
+    SCOUT
+
+    Scout's fight quick battles, inflicting fatal damage before their
+    enemies have a chance to hurt them back.
+
+    Class Skills:
+    Sharper Edge - Increases your ATK
+    Focus Attack - Attacks while increasing your LUK
+    Haste - Increases your AGI
+    ==========
+
+    Do you want to be a SCOUT? (y/n):
+    >>> player_input = "y" #doctest: +SKIP
+    >>> print(test_character["skill_class"]) #doctest: +SKIP
+    scout
     """
     confirm_class = False
     while not confirm_class:
@@ -110,7 +180,7 @@ def choose_class(character):
             print("\n\n\n"
                   "==========\n" +
                   selected_class.upper() + "\n"
-                                           "\n" +
+                  "\n" +
                   get.class_list(selected_class)["desc"] +
                   "==========\n")
             while True:
@@ -137,9 +207,23 @@ def confirm_character(character):
     :return: if the player confirms their character, return True
     :return: if the player denies their character, return False
 
-    >>> test_character = get.blank_character()
-    >>> confirm_character(test_character)
-    {}
+    >>> test_character = get.blank_character() #doctest: +SKIP
+    >>> test_character["kingdom"] = "Hyrule" #doctest: +SKIP
+    >>> test_character["name"] = "Zelda" #doctest: +SKIP
+    >>> test_character["species"] = "elf" #doctest: +SKIP
+    >>> test_character["skill_class"] = "mage" #doctest: +SKIP
+    >>> confirm_character(test_character) #doctest: +SKIP
+    ==========
+    Kingdom: Hyrule
+    Name: Tohru
+
+    Species: Elf
+    Class: Mage
+    ==========
+
+    Is this the character you want to create? (y/n):
+    >>> player_input = "y" #doctest: +SKIP
+    True
     """
     confirm_new_character = False
     while not confirm_new_character:
@@ -168,9 +252,13 @@ def name_bbeg(bbeg):
     :precondition: bbeg must be a dictionary from get.unnamed_bbeg function
     :postcondition: change the value of key "name" to player input
 
-    >>> test_bbeg = get.unnamed_bbeg()
-    >>> name_bbeg(test_bbeg)
-    ''
+    >>> test_bbeg = get.unnamed_bbeg() #doctest: +SKIP
+    >>> name_bbeg(test_bbeg) #doctest: +SKIP
+    Before you embark, remind me,
+    What is the name of your usurper?:
+    >>> player_input = "Ganondorf" #doctest: +SKIP
+    >>> print(test_bbeg["name"]) #doctest: +SKIP
+    Ganondorf
     """
     while True:
         print("Before you embark, remind me,")
@@ -182,10 +270,57 @@ def name_bbeg(bbeg):
             print("\n\n\nPlease enter a name for your enemy!")
 
 
+def confirm_bbeg_name(bbeg):
+    """
+    Ask player to confirm the bbeg's name.
+
+    :param bbeg: a dictionary
+    :precondition: bbeg must be a dictionary from get.unnamed_bbeg function
+    :postcondition: prompt the player to confirm their bbeg
+    :return: if the player confirms their character, return True
+    :return: if the player denies their character, return False
+
+    >>> test_bbeg = get.unnamed_bbeg() #doctest: +SKIP
+    >>> test_bbeg["name"] = "Ganondorf" #doctest: +SKIP
+    Is GANONDORF the name you swear vengence against? (y/n):
+    >>> player_input = "y" #doctest: +SKIP
+    True
+    """
+    confirm_new_character = False
+    while not confirm_new_character:
+        confirm = input("Is " + bbeg["name"].upper() + " the name you swear vengence against? (y/n): ").strip().lower()
+        if confirm == "y":
+            return True
+        elif confirm == "n":
+            return False
+        else:
+            print("\n\n\nPlease confirm your character information.")
+
+
 def main():
     """
     Drive the program.
     """
+    bbeg = get.unnamed_bbeg()
+    confirm_create = False
+    while not confirm_create:
+        bbeg = get.unnamed_bbeg()
+        name_bbeg(bbeg)
+        confirm_create = confirm_bbeg_name(bbeg)
+
+    pprint(bbeg)
+
+    character = get.blank_character()
+    confirm_create = False
+    while not confirm_create:
+        character = get.blank_character()
+        name_kingdom(character)
+        name_character(character)
+        choose_species(character)
+        choose_class(character)
+        confirm_create = confirm_character(character)
+
+    pprint(character)
 
 
 if __name__ == "__main__":
