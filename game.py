098acbf1c95b96ecb47b182fd99e04dc60f25219
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-25 22:10:57 -0800

    Move code to create_new_character for code legibility

--- game.py
+++ game.py
@@ -9,33 +9,38 @@ from pprint import pprint
 from modules import create_character, get
 
 
+def create_new_character(character, bbeg):
+    """
+    Drive the character creation.
+    """
+    confirm_create = False
+    while not confirm_create:
+        print("\n\n")
+        create_character.name_kingdom(character)
+        print("\n\n")
+        create_character.name_character(character)
+        print("\n\n")
+        create_character.choose_species(character)
+        print("\n\n")
+        create_character.choose_class(character)
+        print("\n\n")
+        confirm_create = create_character.confirm_character(character)
+
+    confirm_create = False
+    while not confirm_create:
+        print("\n\n")
+        create_character.name_bbeg(bbeg)
+        confirm_create = create_character.confirm_bbeg_name(bbeg)
+
+
 def game():
     """
     Drive the game.
     """
-    player_character = {}
-    bbeg = {}
+    player_character = get.blank_character()
+    bbeg = get.unnamed_bbeg()
 
-    confirm_create = False
-    while not confirm_create:
-        player_character = get.blank_character()
-        print("\n\n")
-        create_character.name_kingdom(player_character)
-        print("\n\n")
-        create_character.name_character(player_character)
-        print("\n\n")
-        create_character.choose_species(player_character)
-        print("\n\n")
-        create_character.choose_class(player_character)
-        print("\n\n")
-        confirm_create = create_character.confirm_character(player_character)
-
-    confirm_create = False
-    while not confirm_create:
-        bbeg = get.unnamed_bbeg()
-        print("\n\n")
-        create_character.name_bbeg(bbeg)
-        confirm_create = create_character.confirm_bbeg_name(bbeg)
+    create_new_character(player_character, bbeg)
 
     pprint(player_character)
     pprint(bbeg)
